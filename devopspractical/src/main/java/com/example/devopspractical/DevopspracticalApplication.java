80b210b1354d16c4d65a0524eee3aea5ac6bb5c5
Levon-007 <leelevon558@gmail.com>
2025-04-22 10:02:17 +0530

    Add files via upload

---
+++ devopspractical/src/main/java/com/example/devopspractical/DevopspracticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopspractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopspracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopspracticalApplication.class, args);
+	}
+
+}
---
+++ devopspractical/src/test/java/com/example/devopspractical/DevopspracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopspractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopspracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
